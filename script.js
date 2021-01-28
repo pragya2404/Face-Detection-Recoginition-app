0728dbe3b45f6c3256843f34a744bb688f78fcd3
pragya2404 <49591103+pragya2404@users.noreply.github.com>
2021-01-29 03:04:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-const video=document.getElementById("video")
+const video=document.getElementById('video')
 
 
 Promise.all([
-    faceapi.nets.tinyFaceDetector.loadFromUri('models'),
-    faceapi.nets.faceLandmark68Net.loadFromUri('models'),
-    faceapi.nets.faceRecognitionNet.loadFromUri('models'),
-    faceapi.nets.faceExpressionNet.loadFromUri('models'),
-    faceapi.nets.ageGenderNet.loadFromUri('models'),
+    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+    faceapi.nets.ageGenderNet.loadFromUri('/models'),
 ]).then(startVideo)
 
 function startVideo(){
     navigator.mediaDevices.getUserMedia({
-        video:true
-    }).then(
-        stream=>(video.srcObject=stream),
-        err=>console.error(err)
-
-    )
+    video: true,
+    audio: false
+  })
+  .then(
+    (cameraStream) => {
+      video.srcObject = cameraStream;
+    }
+  )
    
 }
 
