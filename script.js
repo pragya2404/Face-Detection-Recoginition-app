4cb82d7ebaecb210f6110daf9d797be5b655da73
pragya2404 <49591103+pragya2404@users.noreply.github.com>
2021-02-02 23:06:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ Promise.all([
     faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
     faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
     faceapi.nets.faceExpressionNet.loadFromUri('/models'),
-    faceapi.nets.ageGenderNet.loadFromUri('/models'),
 ]).then(startVideo)
 
 function startVideo(){
@@ -28,16 +27,12 @@ video.addEventListener('play',()=>{
     const displaySize={width:video.width,height:video.height}
     faceapi.matchDimensions(canvas,displaySize)
     setInterval(async ()=>{
-        const detections=await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions().withAgeAndGender()
+        const detections=await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
         const resizedDetections=faceapi.resizeResults(detections,displaySize)
         canvas.getContext('2d').clearRect(0,0,canvas.width,canvas.height)
         faceapi.draw.drawDetections(canvas,resizedDetections)
         faceapi.draw.drawFaceLandmarks(canvas,resizedDetections)
         faceapi.draw.drawFaceExpressions(canvas,resizedDetections)
-        resizedDetections.forEach( detection => {
-              const box = resizedDetections.detection.box
-              const drawBox = new faceapi.draw.DrawBox(box, { label: Math.round(detection.age) + " year old " + detection.gender })
-              drawBox.draw(canvas)
-    })        // console.log(detections)
+       
     },100)
 })
